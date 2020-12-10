3384f3d54d28d1f07c54782f9356658dfc0d9713
ohaluminum <lejinghuang2000@gmail.com>
2020-12-10 14:22:20 -0600

    1. Setup Environment

---
+++ Models/Person.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Personal_Information_Management.Models
+{
+    public class Person
+    {
+    }
+}
